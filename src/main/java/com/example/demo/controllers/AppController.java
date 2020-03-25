0cae572ed39576d78f8966539a8b60d25f7d2703
SohamRoyNoel <soham.roy.developer@gmail.com>
2020-03-25 23:47:16 +0530

    Profiles - application.properties

---
+++ src/main/java/com/example/demo/controllers/AppController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+
+	@Value("${message}")
+	public String msg;
+	
+	@ResponseBody
+	@GetMapping("index")
+	public ModelAndView getIndexPage() {
+		
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("message", msg);
+		return mv;
+	}
+	
+}
